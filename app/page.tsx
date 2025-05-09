2faf611eb09de7b5d648ba5d1fe189cf13313708
Prachi Deopa <deopaprachi60@gmail.com>
2025-05-09 12:35:08 +0530

    updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import BeforeLogin from "./student/auth/before-login/page"
 import Navbar from "./student/common/navbar"
 export default function Home() {
-  return <div className="bg-gray-100 container">
+  return <div className="container">
     <Navbar />
     <BeforeLogin />
   </div>
