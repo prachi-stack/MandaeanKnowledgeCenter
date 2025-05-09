b155cddc09944278a488efb40af4632b163513b2
Prachi Deopa <deopaprachi60@gmail.com>
2025-05-09 15:38:52 +0530

    updates

--- app/teacher/(components)/my-course/page.tsx
+++ app/teacher/(components)/my-course/page.tsx
@@ -3,23 +3,15 @@ import StatsCards from './components/stats-cards'
 import TotalCourses from '../dashboard/components/total-courses'
 import TopPerformCourse from '../dashboard/components/top-performing-courses'
 import TopStudentLocation from '../dashboard/components/top-student-loc'
-import CourseTable from './components/course-status'
-import PageChange from './components/pagination'
-import RecordsSelector from './components/view'
+import { CourseTable } from './components/course-status/course-table'
 
 const page = () => {
   return (
     <div className='flex space-x-8'>
-      <div className='flex-[2]'>
+      <div className='flex-[2] space-y-8'>
         <StatsCards />
         <CourseTable />
-        <div className="flex justify-between items-center w-full mt-4">
-          <RecordsSelector />
-          <div className="ml-auto">
-            <PageChange />
-          </div>
-        </div>
-      </div>
+       </div>
       <div className='flex-[1] flex flex-col space-y-4'>
         <TotalCourses />
         <TopPerformCourse />
